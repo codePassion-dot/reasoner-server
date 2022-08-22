70f77dc780119a5619629e72faa76f7d3013f411
Hans Rivera <57734041+hrivera7777@users.noreply.github.com>
2022-08-21 19:26:17 -0500

    fix(network): fix issues with cors and cookies (#8)
    - fix cookies issues
    - fix cors issues with origin and credentials
    
    Co-authored-by: codePassion-dot <jjjaramilloar@unal.edu.co>

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from './utils/validation.pipe';
 import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get<ConfigService>(ConfigService);
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
-  app.enableCors();
+  app.enableCors({
+    origin: [configService.get('FRONTEND_URL')],
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
